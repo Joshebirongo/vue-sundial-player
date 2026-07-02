d7d080f7cec224e9c6e4f7b1a132fa36e7e32e05
Jonathan Shebirongo <jonathanshebirongo07@gmail.com>
2026-07-02 17:22:49 +0200

    App component updated

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <script setup>
-
+// Import the application components
+import Navbar from "./components/Navbar.vue";
+// Import the Track player logic module
+import usePlayer from "./composables/usePlayer.js"
+// Import the global application CSS styles
+import "./styles/global.css";
+// Import the App component CSS styles
+import "./styles/App.css";
 </script>
 
 <template>
-  <HelloWorld />
+  <div class="app-shell">
+    <Navbar />
+    <main class="app-main">
+        <RouterView />
+    </main>
+  </div>
 </template>
